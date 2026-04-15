6f4ef135ea06b65b76ba07bde0c1d8af10d44c95
gms2006 <geoffmiguil05@gmail.com>
2026-04-15 14:39:47 +0800

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -1,9 +1,17 @@
 // FILE I/O CLASS
 package AppointmentSchedulingSystem;
+import java.io.PrintWriter;
 
 public class FileManager {
     // TODO: Field: filename String
     
     // Method: saveToFile(Appointment[] list, int count) [Geoff]
     // TODO: Logic: Use PrintWriter/FileWriter to write array to .txt file
+    saveToFile(Appointment[] list, int count){
+        int o = count;
+        PrintWriter outFile = new PrintWriter("Schedule");
+			
+    	outFile.println(Appointment[o]);
+		
+    
 }
