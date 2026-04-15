f5caac41087344303ac8838ae79bce5a4bfeefc0
brulida-code <brulida@addu.edu.ph>
2026-04-15 20:30:00 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,23 +2,18 @@
 package AppointmentSchedulingSystem;
 
 public class Menu {
-  public int menuChoice = 0;
+   // TODO: Field: Scanner [BJ]
+    
+    // Method: display() [BJ]
+    // TODO: Logic: Print the 1-7 menu options
+    
+    // Method: getChoice() [BJ]
+    // TODO: Logic: Read integer, handle invalid inputs
+    
+    // Method: getInput(prompt) [BJ]
+    // TODO: Logic: Print prompt, return user's string input
 
-    //constructor for some reason
-Menu(int menuChoice){
-this.menuChoice = menuChoice;
-}
-
-    public static void display(){
-       System.out.println("---MENU---");
-           System.out.println("1.Add Appointment");
-           System.out.println("2.View Appointments");
-           System.out.println("3.Check Conflicts");
-           System.out.println("4.Cancel Appointments");
-           System.out.println("5.Sort Appointments");
-           System.out.println("6.Save Schedule");
-           System.out.println("7.Exit");
-    }
+    
 
     
 
