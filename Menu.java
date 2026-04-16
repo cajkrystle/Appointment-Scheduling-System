c16ae608ceabd53325532b3d03e1411f6d45b471
brulida-code <brulida@addu.edu.ph>
2026-04-16 09:09:22 +0800

    Implement menu display with options
    Added Scanner field and updated display method with menu options.

--- Menu.java
+++ Menu.java
@@ -1,18 +1,43 @@
 // UI / INPUT CLASS
 package AppointmentSchedulingSystem;
-
+  import java.util.Scanner;
 public class Menu {
-   // TODO: Field: Scanner [BJ]
+   
+  Scanner scanner = new Scanner(System.in);
     
     // Method: display() [BJ]
     // TODO: Logic: Print the 1-7 menu options
-    
+
+   public void display(){
+       System.out.println("-----MENU OPTIONS-----");
+      System.out.println("1.Add Appointments");
+      System.out.println("2.View Appointments");
+      System.out.println("3.Check Conflicts");
+      System.out.println("4.Cancel Appointments");
+      System.out.println("5.Sort Appointments");
+      System.out.println("6.Save Schedule");
+      System.out.println("7.Exit");
+     
+
+
+   }
+ 
+   
     // Method: getChoice() [BJ]
     // TODO: Logic: Read integer, handle invalid inputs
-    
+      public int getChoice(){
+
+
+        
+      }
+   
     // Method: getInput(prompt) [BJ]
     // TODO: Logic: Print prompt, return user's string input
+      public String getInput(){
 
+
+
+      }
     
 
     
