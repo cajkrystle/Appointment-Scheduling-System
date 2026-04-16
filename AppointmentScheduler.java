9cd62998869cf8680a2077cf2b9575c39ad3fa76
gms2006 <geoffmiguil05@gmail.com>
2026-04-17 07:48:14 +0800

    Update AppointmentScheduler.java

--- AppointmentScheduler.java
+++ AppointmentScheduler.java
@@ -1,14 +1,17 @@
 // CORE LOGIC CLASS
 package AppointmentSchedulingSystem;
+import java.util.Scanner;
 
 public class AppointmentScheduler {
     // TODO: Fields: Appointment[] array, int count, static int MAX_SIZE
-    String[] appointments = new String[];
-    int count = 0;
-    int MAX_SIZE;
+    private String[] appointments = new String[];
+    private int count;
+    private int MAX_SIZE;
     // TODO: Constructor: initialize array and count
     AppointmentScheduler {
         
+        this.count = 0;
+        this.appointments = new String[]
         this.MAX_SIZE = 100;
         
     }
@@ -16,15 +19,27 @@ public class AppointmentScheduler {
     
     // Method: addAppointment(name, date, time) [Geoff]
     // TODO: Logic: Create new Appointment object, save to array, increment count
-    static int addAppointment(String name, String date, String time){
+    static int addAppointment(){
+        
+        Scanner scan = new Scanner(System.in);
         
         if(count == MAX_SIZE){
             System.out.println("Error: Reached limit already");
             break;
         }
         
+        System.out.println("Enter Date:");
+        String date = scan.nextLine;
+        System.out.println("Enter Time:");
+        String time = scan.nextLine;
+        System.out.println("Enter Name:");
+        String name = scan.nextLine;
+        
+        Appointment appoint = new Appointment(name, date, time);
+        
         appointments[count].date = date;
         appointments[count].time = time;
+        appointments[count].name = name;
         
         count++;
         
@@ -76,6 +91,36 @@ public class AppointmentScheduler {
     
     // Method: cancelAppointment(name) [Geoff]
     // TODO: Logic: Find index of name, remove it, and shift array items left
+    static int cancelAppointment(){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter Time:");
+        String time = scan.nextLine;
+        System.out.println("Enter Name:");
+        String name = scan.nextLine;
+            
+        for (int i = 0; i < count; i++) {
+            if(appointments[i].date.equals(date) && appointments[i].time.equals(time)){
+                appointments[i].date = null;
+                appointments[i].time = null;
+                appointments[i].name = null;
+
+                
+                for (int j = i; j < count; j++){
+                    appointments[j].date = appointments[j+1].date;
+                    appointments[j].time = appointments[j+1].time;
+                    appointments[j].name = appointments[j+1].name;
+                }
+                
+            }
+                
+        }
+        
+            
+        count--;
+        
+    }
     
     // Method: sortAppointments() [Geoff]
     // TODO: Logic: Swap-sort algorithm to order appointments
