e125e63f26410f79eb7af41a040d043ca9849d89
cajkrystle <cajkrystle@gmail.com>
2026-04-16 22:29:57 +0800

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -3,11 +3,24 @@ package AppointmentSchedulingSystem;
 
 public class Appointment {
     // TODO: Fields: name, date, time [AJ]
+    private String name;
+    private String date;
+    private String time;
     
     // TODO: Constructor: initialize the fields
+    public Appointment(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
     
     // TODO: Getters: getName(), getDate(), getTime() [AJ]
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public String getTime() { return time; }
     
     // TODO: Method: toString() 
-    // Return formatted string for display
+    public String toString() {
+        return String.format("%-15s | %-12s | %-8s", name, date, time);
+    }
 }
