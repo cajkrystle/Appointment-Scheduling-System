3313877451410764b647d323f56d50b76e27a7c4
cajkrystle <cajkrystle@gmail.com>
2026-04-20 15:46:03 +0800

    Refactor Appointment class to use day and slot indices

--- Appointment.java
+++ Appointment.java
@@ -1,26 +1,37 @@
-// DATA OBJECT CLASS
+// APPOINTMENT CLASS
+// 4-305 [Castro, Rulida, Salcedo, Timbal]
+
 package AppointmentSchedulingSystem;
 
 public class Appointment {
-    // TODO: Fields: name, date, time [AJ]
     private String name;
-    private String date;
-    private String time;
-    
-    // TODO: Constructor: initialize the fields
-    public Appointment(String name, String date, String time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
+    private int    dayIndex;   // 0=Sunday ... 6=Saturday
+    private int    slotIndex;  // 0=7-9AM, 1=9AM-12PM, 2=1-3PM, 3=3-5PM 
+
+    public static final String[] DAY_NAMES = {
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    };
+    public static final String[] SLOT_LABELS = {
+        "7:00 AM - 9:00 AM",
+        "9:00 AM - 12:00 PM",
+        "1:00 PM - 3:00 PM",
+        "3:00 PM - 5:00 PM"
+    };
+
+    public Appointment(String name, int dayIndex, int slotIndex) {
+        this.name      = name;
+        this.dayIndex  = dayIndex;
+        this.slotIndex = slotIndex;
     }
-    
-    // TODO: Getters: getName(), getDate(), getTime() [AJ]
-    public String getName() { return name; }
-    public String getDate() { return date; }
-    public String getTime() { return time; }
-    
-    // TODO: Method: toString() 
+
+    public String getName()      { return name; }
+    public int    getDayIndex()  { return dayIndex; }
+    public int    getSlotIndex() { return slotIndex; }
+    public String getDate()      { return DAY_NAMES[dayIndex]; }
+    public String getTime()      { return SLOT_LABELS[slotIndex]; }
+
+    @Override
     public String toString() {
-        return String.format("%-15s | %-12s | %-8s", name, date, time);
+        return String.format("%-20s | %-12s | %s", name, getDate(), getTime());
     }
 }
